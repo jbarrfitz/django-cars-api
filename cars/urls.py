f82645be0d9b5a67959ce0c0c519db16048efec0
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-07-06 23:28:46 -0400

    feat: Migrated; added docker files

--- cars/urls.py
+++ cars/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CarList, CarDetail
 
 urlpatterns = [
-    path("", CarList.as_view(), name="car_list"),
-    path("<pk:id>/", CarDetail.as_view(), name="car_detail")
+    path("", CarList.as_view()),
+    path("<int:pk>/", CarDetail.as_view())
 ]
